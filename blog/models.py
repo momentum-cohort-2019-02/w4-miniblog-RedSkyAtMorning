c4a346a110d36c01a6da3d5f94c6d5667c9e7c21
Jay Spain <meanderingjasmine@gmail.com>
2019-03-09 16:51:40 -0500

    skeleton

---
+++ blog/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import User
+from datetime import date
+
+# Create your models here.
+
+
+
+
+
+
+
---
+++ blog/urls.py
@@ -0,0 +1,7 @@
+## This is catalog.urls
+
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+
